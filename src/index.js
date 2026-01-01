e86d50b05d9a82c0f36a11907f1898d595518faa
Taleb-2025 <bental.herne@gmail.com>
2026-01-01 23:32:03 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
 
+import express from "express";
+
+const app = express();
+const PORT = process.env.PORT;
+
+app.get("/", (req, res) => {
+  res.send("API IS RUNNING");
+});
+
+app.get("/api/status", (req, res) => {
+  res.json({ ok: true, time: Date.now() });
+});
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log("SERVER STARTED ON PORT", PORT);
+});
