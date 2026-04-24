1fa94cc4d6d1c8c8ea997a7666283ce2466e2a58
Taleb-2025 <bental.herne@gmail.com>
2026-04-24 13:02:53 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import express from "express"
 import cors from "cors"
-import flowRouter          from "./api/flow.js"
-import visionageRoute      from "./api/visionage.route.js"
-import visionageTrackRoute from "./api/visionage-track.route.js"
-import blackHoleRoute      from "./api/visionage-black-hole.route.js"
-import visionRoute         from "./api/vision.route.js"
-import automotiveRoute     from "./api/automotive.route.js"
+import flowRouter     from "./api/flow.js"
+import visionageRoute from "./api/visionage.route.js"
+import visionRoute    from "./api/vision.route.js"
+import automotiveRoute from "./api/automotive.route.js"
+import cameraRoute    from "./api/camera.route.js"
+import securityRoute  from "./api/cam.security.route.js"
 
 const app  = express()
 const PORT = process.env.PORT || 8080
@@ -30,12 +30,12 @@ app.use(express.raw({
 }))
 app.use(express.static("public"))
 
-app.use("/api/flow",            flowRouter)
-app.use("/api/visionage",       visionageRoute)
-app.use("/api/visionage-track", visionageTrackRoute)
-app.use("/api/black-hole",      blackHoleRoute)
-app.use("/api/vision",          visionRoute)
-app.use("/api/automotive",      automotiveRoute)
+app.use("/api/flow",       flowRouter)
+app.use("/api/visionage",  visionageRoute)
+app.use("/api/vision",     visionRoute)
+app.use("/api/automotive", automotiveRoute)
+app.use("/api/camera",     cameraRoute)
+app.use("/api/security",   securityRoute)
 
 app.get("/", (_req, res) => {
   res.json({
