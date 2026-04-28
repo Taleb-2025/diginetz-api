f9d9e626474daf4b548b4687295b8e655705601c
Taleb-2025 <bental.herne@gmail.com>
2026-04-28 16:13:01 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,6 @@
 import express from "express"
 import cors from "cors"
-import flowRouter      from "./api/flow.js"
-import visionageRoute  from "./api/visionage.route.js"
-import visionRoute     from "./api/vision.route.js"
-import automotiveRoute from "./api/automotive.route.js"
-import cameraRoute     from "./api/camera.route.js"
-import securityRoute   from "./api/cam.security.route.js"
+
 import cycleguardRoute from "./api/cycleguard.route.js"
 import cycleguardSessionRoute from "./api/cycleguard-session.route.js"
 import identityRoute from "./api/identity.route.js"
@@ -30,12 +25,7 @@ app.use(express.json({ limit: "10mb" }))
 app.use(express.raw({ type: "application/octet-stream", limit: "1mb" }))
 app.use(express.static("public"))
 
-app.use("/api/flow",       flowRouter)
-app.use("/api/visionage",  visionageRoute)
-app.use("/api/vision",     visionRoute)
-app.use("/api/automotive", automotiveRoute)
-app.use("/api/camera",     cameraRoute)
-app.use("/api/security",   securityRoute)
+
 app.use("/api/cycleguard", cycleguardRoute)
 app.use("/api/cycleguard-session", cycleguardSessionRoute)
 app.use("/api/identity", identityRoute)
